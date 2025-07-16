b202cacdcd388766e6078b6870a723c27d378f0f
schubert-da <schubertdeabreu2@gmail.com>
2025-07-16 23:05:52 +0530

    adds sections

---
+++ src/lib/components/Sections/BodySection.svelte
@@ -0,0 +1,82 @@
+<script>
+	let { children } = $props();
+</script>
+
+<section class="body-section">
+	<div class="title-container">
+		<div class="title-row">
+			<div class="chapter-number">
+				<p>1</p>
+			</div>
+
+			<h2 class="title">Competing in Kaggle Competitions don't make you a better Data Scientist</h2>
+		</div>
+		<div class="subtitle-row">
+			<p class="subtitle">... But that depends on how you define “competing”</p>
+		</div>
+	</div>
+
+	<div class="text-container">
+		{@render children()}
+	</div>
+</section>
+
+<style lang="scss">
+	section.body-section {
+		width: 100%;
+
+		.title-container {
+			margin-bottom: 80px;
+		}
+
+		.title-row {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: center;
+			gap: 2rem;
+
+			padding-bottom: 16px;
+			border-bottom: 2px solid #000;
+
+			.chapter-number {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				justify-content: center;
+
+				width: 96px;
+				height: 96px;
+				min-width: 96px;
+				min-height: 96px;
+				border: 1px solid #838383;
+				border-radius: 8px;
+
+				font-family: 'Segoe UI';
+				font-size: 64px;
+				font-weight: 600;
+				color: #fff;
+				background-color: #ddbbc2;
+				stroke: #000;
+				stroke-width: 1px;
+			}
+		}
+
+		.subtitle-row {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: flex-end;
+
+			padding: 10px 12px;
+			border-bottom: 1px solid #444;
+		}
+
+		.text-container {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			gap: 1.5rem;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import BodySection from '$lib/components/Sections/BodySection.svelte';
+</script>
+
+<div class="hero-section mb-8">
+	<h1>Why Should I Kaggle</h1>
+	<p>How Kaggle benefits the overall data science and ML community</p>
+</div>
+
+<div class="body-section">
+	<BodySection>
+		<p>
+			Kaggle is a great place to learn for newcomers and data science enthusiasts - learning
+			competitions to take your first steps, integrated environments that make spinning up a model
+			easier than ever, interesting problem statements to experiment on different domains and
+			learning through collaboration and friendly competition.
+		</p>
+
+		<p>
+			But it still gets a lot of flak because it isn't a mirror to working on actual data science
+			problems in the real world. Experts contend that datasets are spoon-fed - skipping any actual
+			data collection and cleaning process, competition problems are not reflective of the typical
+			day-to-day work of a data scientist, sometimes the quality of the data is poor or it's hard to
+			source in real projects which is never the case in Kaggle projects, timelines are unrealistic
+			in comparison to real-life projects.
+		</p>
+	</BodySection>
+</div>
+
+<style lang="scss">
+</style>
